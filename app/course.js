d65d9ef2e17ddf05b1ed91ba218eecec5ea25912
Nathan Menge <mengen@turnitin.com>
2017-05-03 22:30:13 -0400

    fixup

--- app/course.js
+++ app/course.js
@@ -10,13 +10,13 @@ export default class Course extends React.Component {
         this.state = {};
     }
 
-    static render() {
+    render() {
         return (
             <div id="everything">
                 <Col md={10} mdOffset={1}>
                     <Jumbotron>
                         <h1>Welcome!</h1>
-                        <p>This is the selenium obstacle course.  Really just a pile of meaningless UI components.  But we will use this to demonstrate the abilities of Selenium Web Driver.</p>
+                        <p>This is the selenium obstacle course. Really just a pile of meaningless UI components.  But we will use this to demonstrate the abilities of Selenium Web Driver.</p>
                     </Jumbotron>
                 </Col>
                 <Col md={6} mdOffset={3}>
